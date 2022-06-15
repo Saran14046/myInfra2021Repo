3a68d028921f79262becfba3261d4bf8cc5bdfcc
Saran14046 <104748503+Saran14046@users.noreply.github.com>
2022-06-15 13:25:34 +0530

    Update backend.tf
    updated commit

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mydev-tf-state-buckets"
     key = "main"
-    region = "us-east-2"
+    region = "eu-west-2"
     dynamodb_table = "my-dynamodb-table"
   }
 }
