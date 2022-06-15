c796c46475d7c2f4ff8c5a2b5626ab01387be11b
Saran14046 <104748503+Saran14046@users.noreply.github.com>
2022-06-15 13:47:57 +0530

    Update sonar.tf
    AMI id commit

--- sonar.tf
+++ sonar.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0b9064170e32bde34"
+      ami           = "ami-073ee45c9f61cbaa3"
 
       key_name = var.key_name
       instance_type = "t2.micro"
